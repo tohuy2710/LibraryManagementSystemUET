dd12cad4ac98e4df346abd7e617eaeeab29e0c45
tohuy2710 <tohuy2710@gmail.com>
2024-10-27 09:27:06 +0700

    update front-end

--- src/main/java/org/example/librarymanagementsystemuet/BookManagementController.java
+++ src/main/java/org/example/librarymanagementsystemuet/BookManagementController.java
@@ -5,9 +5,14 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 import javafx.util.Callback;
 import package1.AlertMessage;
 import package1.Book;
@@ -19,16 +24,31 @@ import java.util.ResourceBundle;
 public class BookManagementController implements Initializable {
 
     @FXML
-    private Button addBookButton;
+    private Button addBookButton11;
 
     @FXML
-    private TableColumn<Book, String> addedDateCol;
+    private Button addBookButton111;
+
+    @FXML
+    private Button addBookButton1111;
+
+    @FXML
+    private TableColumn<?, ?> addedDateCol;
+
+    @FXML
+    private Circle adminAvatar;
+
+    @FXML
+    private HBox adminInfoBox;
+
+    @FXML
+    private Label adminName;
 
     @FXML
     private TableColumn<Book, String> authorsCol;
 
     @FXML
-    private AnchorPane bookManagementPant;
+    private HBox bookManagementMainBox;
 
     @FXML
     private TableColumn<Book, String> bookNameCol;
@@ -37,13 +57,46 @@ public class BookManagementController implements Initializable {
     private TableView<Book> bookTable;
 
     @FXML
+    private Button booksManagementButton;
+
+    @FXML
+    private Button booksManagementButtonMinimize;
+
+    @FXML
+    private Button changeMenuSize;
+
+    @FXML
+    private AnchorPane contentPane;
+
+    @FXML
+    private Button dashboardButton;
+
+    @FXML
+    private Button dashboardButtonMinimize;
+
+    @FXML
     private TableColumn<Book, String> idCol;
 
     @FXML
     private TableColumn<Book, String> isbnCol;
 
     @FXML
-    private TableColumn<Book, String> publisherCol;
+    private Button logoutButton;
+
+    @FXML
+    private Button logoutButtonMinimize;
+
+    @FXML
+    private AnchorPane paneMenuFull;
+
+    @FXML
+    private AnchorPane paneMenuMini;
+
+    @FXML
+    private TableColumn<?, ?> publisherCol;
+
+    @FXML
+    private HBox searchBookResultBox;
 
     @FXML
     private Button searchButton;
@@ -54,25 +107,82 @@ public class BookManagementController implements Initializable {
     @FXML
     private ChoiceBox<String> selectTypeSearch;
 
+    @FXML
+    private Label topBorrowedBook1Author;
+
+    @FXML
+    private Label topBorrowedBook1BrrowedCount;
+
+    @FXML
+    private HBox topBorrowedBook1HBox;
+
+    @FXML
+    private Label topBorrowedBook1ISBN;
+
+    @FXML
+    private ImageView topBorrowedBook1Image;
+
+    @FXML
+    private Label topBorrowedBook1Name;
+
+    @FXML
+    private Label topBorrowedBook2Author;
+
+    @FXML
+    private Label topBorrowedBook2BrrowedCount;
+
+    @FXML
+    private HBox topBorrowedBook2HBox;
+
+    @FXML
+    private Label topBorrowedBook2ISBN;
+
+    @FXML
+    private ImageView topBorrowedBook2Image;
+
+    @FXML
+    private Label topBorrowedBook2Name;
+
+    @FXML
+    private Label topBorrowedBook3Author;
+
+    @FXML
+    private Label topBorrowedBook3BrrowedCount;
+
+    @FXML
+    private HBox topBorrowedBook3HBox;
+
+    @FXML
+    private Label topBorrowedBook3ISBN;
+
+    @FXML
+    private ImageView topBorrowedBook3Image;
+
+    @FXML
+    private Label topBorrowedBook3Name;
+
+    @FXML
+    private Button usersManagementButton;
+
+    @FXML
+    private Button usersManagementButtonMinimize;
+
     private String[] typeSearch = {"ID", "ISBN", "Name"};
 
     private ObservableList<Book> bookList = FXCollections.observableArrayList();
 
-
-    public Connection connectDB() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager
-                    .getConnection("jdbc:mysql://localhost:3307/library_management_system_uet",
-                            "root", "");
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+    public void showBookManagementMainBox(ActionEvent event) {
+        bookManagementMainBox.setVisible(true);
+        searchBookResultBox.setVisible(false);
     }
 
     public void searchBooks(ActionEvent event) {
         try {
-            Database.connect = connectDB();
+            bookManagementMainBox.setVisible(false);
+            searchBookResultBox.setVisible(true);
+
+
+            Database.connect = Database.connectDB();
             String searchQuery = null;
             String searchValue = searchTextField.getText();
 
@@ -104,9 +214,7 @@ public class BookManagementController implements Initializable {
                     book.setAddedDate(Database.result.getString("addedDate"));
                     bookList.add(book);
                 }
-            }
-
-            else {
+            } else {
                 AlertMessage alertMessage = new AlertMessage();
                 alertMessage.errorMessage("Please enter a search value!");
             }
@@ -115,12 +223,12 @@ public class BookManagementController implements Initializable {
         }
 
         // Bind table columns to the properties of the Book class
-        idCol.setCellValueFactory(new PropertyValueFactory<Book, String>("id"));
-        isbnCol.setCellValueFactory(new PropertyValueFactory<Book, String>("isbn"));
-        bookNameCol.setCellValueFactory(new PropertyValueFactory<Book, String>("name"));
-        authorsCol.setCellValueFactory(new PropertyValueFactory<Book, String>("author"));
-        publisherCol.setCellValueFactory(new PropertyValueFactory<Book, String>("publisher"));
-        addedDateCol.setCellValueFactory(new PropertyValueFactory<Book, String>("addedDate"));
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        isbnCol.setCellValueFactory(new PropertyValueFactory<>("isbn"));
+        bookNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        authorsCol.setCellValueFactory(new PropertyValueFactory<>("author"));
+        publisherCol.setCellValueFactory(new PropertyValueFactory<>("publisher"));
+        addedDateCol.setCellValueFactory(new PropertyValueFactory<>("addedDate"));
 
         Callback<TableColumn<Book, String>, TableCell<Book, String>> cellFactory = (param) -> {
             final TableCell<Book, String> cell = new TableCell<Book, String>() {
@@ -148,11 +256,102 @@ public class BookManagementController implements Initializable {
         bookTable.setItems(bookList);
     }
 
+    public void changeMenuSize(ActionEvent event) {
+        if (paneMenuFull.isVisible()) {
+            paneMenuFull.setVisible(false);
+            paneMenuMini.setVisible(true);
+        } else {
+            paneMenuFull.setVisible(true);
+            paneMenuMini.setVisible(false);
+        }
+    }
 
+    public void getTopBorrowedBooks(ActionEvent event) {
+        try {
+            Database.connect = Database.connectDB();
+            String query = "SELECT bookid, COUNT(userid) AS cnt, " +
+                    "books.linkCoverImage, books.name, books.author, books.isbn " +
+                    "FROM borrowbooks " +
+                    "LEFT JOIN books ON books.id = borrowbooks.bookid " +
+                    "GROUP BY bookid " +
+                    "ORDER BY cnt DESC " +
+                    "LIMIT 3;";
+            Database.prepare = Database.connect.prepareStatement(query);
+            Database.result = Database.prepare.executeQuery();
+
+            int i = 1;
+            while (Database.result != null && Database.result.next()) {
+                if (i == 1) {
+                    topBorrowedBook1Name.setText(Database.result.getString("name"));
+                    topBorrowedBook1Author.setText(Database.result.getString("author"));
+                    topBorrowedBook1ISBN.setText(Database.result.getString("isbn"));
+                    topBorrowedBook1BrrowedCount.setText(Database.result.getString("cnt"));
+                    topBorrowedBook1Image.setImage(new Image(Database.result.getString("linkCoverImage")));
+                } else if (i == 2) {
+                    topBorrowedBook2Name.setText(Database.result.getString("name"));
+                    topBorrowedBook2Author.setText(Database.result.getString("author"));
+                    topBorrowedBook2ISBN.setText(Database.result.getString("isbn"));
+                    topBorrowedBook2BrrowedCount.setText(Database.result.getString("cnt"));
+                    topBorrowedBook2Image.setImage(new Image(Database.result.getString("linkCoverImage")));
+                } else if (i == 3) {
+                    topBorrowedBook3Name.setText(Database.result.getString("name"));
+                    topBorrowedBook3Author.setText(Database.result.getString("author"));
+                    topBorrowedBook3ISBN.setText(Database.result.getString("isbn"));
+                    topBorrowedBook3BrrowedCount.setText(Database.result.getString("cnt"));
+                    topBorrowedBook3Image.setImage(new Image(Database.result.getString("linkCoverImage")));
+                }
+                i++;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         selectTypeSearch.getItems().addAll(typeSearch);
         selectTypeSearch.setValue("ID");
+
+        getTopBorrowedBooks(null);
+
+        paneMenuFull.visibleProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue && paneMenuMini.isVisible()) {
+                paneMenuMini.setVisible(false);
+            }
+            adjustContentSize();
+        });
+
+        paneMenuMini.visibleProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue && paneMenuFull.isVisible()) {
+                paneMenuFull.setVisible(false);
+            }
+            adjustContentSize();
+        });
+
+        contentPane.widthProperty().addListener((observable, oldValue, newValue) -> {
+            adjustHBoxSize();
+        });
     }
+
+    private void adjustContentSize() {
+        if (paneMenuFull.isVisible() && !paneMenuMini.isVisible()) {
+            AnchorPane.setLeftAnchor(contentPane, 240.0); // Giữ tỷ lệ kích thước cho menu lớn
+        } else if (!paneMenuFull.isVisible() && paneMenuMini.isVisible()) {
+            AnchorPane.setLeftAnchor(contentPane, 80.0); // Giữ tỷ lệ kích thước cho menu nhỏ
+        }
+        AnchorPane.setRightAnchor(contentPane, 0.0); // Cố định bên phải để tự điều chỉnh kích thước
+
+        contentPane.applyCss();
+        adjustHBoxSize();
+    }
+
+    private void adjustHBoxSize() {
+        for (Node node : contentPane.getChildren()) {
+            if (node instanceof HBox) {
+                ((HBox) node).prefWidthProperty().bind(contentPane.widthProperty());
+            }
+        }
+    }
+
 }
